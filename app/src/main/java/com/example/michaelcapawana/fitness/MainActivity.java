7f7696c4804911a57371cb81c2e9c63f48d5c435
Gwinert <gwinert@gmail.com>
2017-02-15 09:43:05 -0700

    I hope this works

--- app/src/main/java/com/example/michaelcapawana/fitness/MainActivity.java
+++ app/src/main/java/com/example/michaelcapawana/fitness/MainActivity.java
@@ -12,4 +12,5 @@ public class MainActivity extends AppCompatActivity {
     }
     // I am going to wreck this code while you're not watching #y'all gunna fail!
     // I hope we all fail this class
+    //test for preston
 }
