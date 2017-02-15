672cd51eb58b731e83fa6092c9b2f58abeae1ade
Gwinert <gwinert@gmail.com>
2017-02-15 09:31:37 -0700

    Turtles are great for my appetite.

--- app/src/main/java/com/example/michaelcapawana/fitness/MainActivity.java
+++ app/src/main/java/com/example/michaelcapawana/fitness/MainActivity.java
@@ -11,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     // I am going to wreck this code while you're not watching #y'all gunna fail!
+    // I hope we all fail this class
 }
