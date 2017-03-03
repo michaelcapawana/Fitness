56b856779b097410ed9045178ede79e29e0457c4
Michael <michaelcapawana@hotmail.com>
2017-03-03 15:23:15 -0700

    graph

---
+++ app/src/main/java/com/example/michaelcapawana/fitness/BMI_Date_Data.java
@@ -0,0 +1,45 @@
+package com.example.michaelcapawana.fitness;
+
+/**
+ * Created by michaelcapawana on 3/3/17.
+ */
+
+public class BMI_Date_Data {
+    long bmi = 0;
+    int year = 0;
+    int month = 0;
+    int day = 0;
+
+    public long getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(long bmi) {
+        this.bmi = bmi;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+}
--- app/src/main/java/com/example/michaelcapawana/fitness/activity_graphs.java
+++ app/src/main/java/com/example/michaelcapawana/fitness/activity_graphs.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.util.Calendar;
+
 public class activity_graphs extends AppCompatActivity {
 
     @Override
@@ -16,11 +18,33 @@ public class activity_graphs extends AppCompatActivity {
     }
 
     public void BMI(View view) {
+        BMI_Date_Data bmiData [];
+        BMI_Date_Data BMIObject = new BMI_Date_Data();
+        BMI_Date_Data BMINull = new BMI_Date_Data();
         SharedPreferences profile = PreferenceManager.getDefaultSharedPreferences(this);
-        float weight = profile.getInt("Weight", 0);
-        float height = profile.getInt("Height", 0);
-        float bmi =  (weight/((height)*(height)) * 703);
+        SharedPreferences bmiFile = getSharedPreferences("bmi", MODE_PRIVATE);
+        SharedPreferences.Editor editor = bmiFile.edit();
+        long weight = profile.getInt("Weight", 0);
+        long height = profile.getInt("Height", 0);
+        int size =  bmiFile.getInt("array_size", 0);
+        bmiData = new BMI_Date_Data[size];
+        for(int i=0; i < size; i++)
+            bmiFile.getAll();
+        if (bmiData) {
+
+        }
+        BMIObject.setBmi((weight/((height)*(height)) * 703));
         Log.d("Graphs", String.valueOf(bmi));
+        Calendar rightnow = Calendar.getInstance();
+        int month = rightnow.getTime().getMonth(); //Months are 0-11
+        //Log.d("Date", String.valueOf(month));
+        int day = rightnow.getTime().getDate();
+        //Log.d("Date", String.valueOf(day));
+
+        editor.putInt("array_size", bmiData.length);
+        for(int i=0;i < bmiData; i++)
+            editor.putString("array_" + i, bmiData[i]);
+        editor.commit();
     }
 
     public void GenerateGraph() {
