58cbecb4cd4841ac5f65de58ad9eb734e0c3524e
Michael <michaelcapawana@hotmail.com>
2017-03-03 14:22:45 -0700

    settings

--- app/src/main/java/com/example/michaelcapawana/fitness/activity_graphs.java
+++ app/src/main/java/com/example/michaelcapawana/fitness/activity_graphs.java
@@ -1,7 +1,11 @@
 package com.example.michaelcapawana.fitness;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class activity_graphs extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class activity_graphs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphs);
+    }
+
+    public void BMI(View view) {
+        SharedPreferences profile = PreferenceManager.getDefaultSharedPreferences(this);
+        float weight = profile.getInt("Weight", 0);
+        float height = profile.getInt("Height", 0);
+        float bmi =  (weight/((height)*(height)) * 703);
+        Log.d("Graphs", String.valueOf(bmi));
+    }
+
+    public void GenerateGraph() {
+
     }
 }
--- app/src/main/java/com/example/michaelcapawana/fitness/activity_userInfo.java
+++ app/src/main/java/com/example/michaelcapawana/fitness/activity_userInfo.java
@@ -1,7 +1,16 @@
 package com.example.michaelcapawana.fitness;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class activity_userInfo extends AppCompatActivity {
 
@@ -10,4 +19,30 @@ public class activity_userInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
     }
+
+    public void saveProfile(View view) {
+        Log.d("MainActivity", "Saving Profile Information");
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        EditText editText3 = (EditText) findViewById(R.id.editText3);
+        EditText editText4 = (EditText) findViewById(R.id.editText4);
+        EditText editText5 = (EditText) findViewById(R.id.editText5);
+        //if(Integer.parseInt(editText2.getText().toString()) > 120 && Integer.parseInt(editText2.getText().toString()) < 0)
+        //{
+          //  throw new AssertionError("Invalid Age");
+        //}
+        Context context = activity_userInfo.this;
+        SharedPreferences profile = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = profile.edit();
+        editor.putString("Name", editText.getText().toString());
+        editor.putInt("Age", Integer.parseInt(editText2.getText().toString()));
+        editor.putInt("Height", Integer.parseInt(editText3.getText().toString()));
+        editor.putInt("Weight", Integer.parseInt(editText4.getText().toString()));
+        editor.putString("Gender", editText5.getText().toString());
+        editor.commit();
+        Log.d("UserInfo", "Saved!");
+        String name = profile.getString("Name", "");
+        Log.d("UserInfo", name);
+    }
+
 }
